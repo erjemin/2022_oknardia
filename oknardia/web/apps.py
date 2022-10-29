e82963f902d61b3a0a1767cb69ff584228d800d4
erjemin <erjemin@gmail.com>
2022-10-30 00:37:30 +0300

    Описание приложения для красивости админки

--- oknardia/web/apps.py
+++ oknardia/web/apps.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy
 
 
 class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
+    verbose_name = gettext_lazy("САЙТ")
