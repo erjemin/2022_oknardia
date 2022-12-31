6a4c315cdc5a6a3d251ef49ac3c5c4b2e3ae275d
erjemin <erjemin@gmail.com>
2022-12-31 04:33:59 +0300

    Вьюшка: Подгружаемых фреймов ценовых предложений -- готово

--- oknardia/oknardia/urls.py
+++ oknardia/oknardia/urls.py
@@ -71,9 +71,14 @@ urlpatterns = [
     re_path(r'^catalog/company[/*]$', catalog.catalog_company),
     re_path(r'^catalog/company/(?P<company_id>\d+)-(?P<company_name_slug>\S*)[/*]$', catalog.catalog_company_detail),
     # ЦЕНОВЫЕ ПРЕДЛОЖЕНИЯ
+    # --- Одиночное окно
     re_path(r'^tsena-odnogo-okna/(?P<win_width_mm>\d+)x(?P<win_height_mm>\d+)mm/tip(?P<win_id>\d+)[/*]$',
             prices.report_one_win_price),
-    re_path(r'^(?P<build_id>\d{1,6})/(?P<apart_id>\d{1,})/(?P<slug>[\s\S]+|.*)$', prices.report_price),
+    # --- Ценовая выдача
+    re_path(r'^(?P<build_id>\d+)/(?P<apart_id>\d+)/(?P<slug>[\s\S]+|.*)[/*]$', prices.report_price),
+    # --- Подгружаемый фрейм ценовая выдачи
+    re_path(r'^next_price_frame/idA(?P<apart_id>\d+)MDPO(?P<mount_dim_per_offer>\d+)LON(?P<address_longitude>\d+)'
+            r'LAT(?P<address_latitude>\d+\.*\d*)N(?P<frame_begin_n>\d+\.*\d*)\S*[/*]$', prices.next_price_frame),
 
 ]
 
