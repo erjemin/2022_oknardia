15abbe73678c26e802dd9fe0412aba2b5a3778e0
erjemin <erjemin@gmail.com>
2026-04-17 16:07:19 +0300

    mod: migrate to poetry and add django debug toolbar for dev

--- oknardia/oknardia/settings.py
+++ oknardia/oknardia/settings.py
@@ -84,6 +84,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Разрешенные IP для отладки (нужно для django-debug-toolbar).
+INTERNAL_IPS = env.list('INTERNAL_IPS', default=['127.0.0.1', 'localhost'])
+
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware', *MIDDLEWARE]
+
 ROOT_URLCONF = 'oknardia.urls'
 
 TEMPLATES = [
