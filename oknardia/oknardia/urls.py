9e2708963833d560731977539ba84bfc43fd51cc
erjemin <erjemin@gmail.com>
2026-04-20 00:54:11 +0300

    minor: в отдельный catalog_profiles.py выделены все вьюхи для каталога профилей

--- oknardia/oknardia/urls.py
+++ oknardia/oknardia/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from oknardia.settings import *
-from web import views, autocomplete_addr, user_manager, blog, diagrams, report1, report2, catalog, prices, service
-
-
+from web import views, autocomplete_addr, user_manager, blog, diagrams, report1, report2, catalog, prices, service, \
+    catalog_profiles
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -57,13 +56,13 @@ urlpatterns = [
     re_path(r'^stat/rating[/*]$', report2.ratings),
     re_path(r'^stat/rating/profiles_rank[/*]$', report2.profiles_rating),
     # --- Каталог
-    # --- --- Каталог профилей
     re_path(r'^catalog[/*]$', catalog.catalog_root),
-    re_path(r'^catalog/profile[/*]$', catalog.catalog_profile),
+    # --- --- Каталог профилей
+    re_path(r'^catalog/profile[/*]$', catalog_profiles.catalog_profile),
     re_path(r'^catalog/profile/(?P<manufacture_id>\d+)-(?P<manufacture_name>\S*)'
-            r'/(?P<model_id>\d+)-(?P<model_name>\S*)[/*]$', catalog.catalog_profile_model),
+            r'/(?P<model_id>\d+)-(?P<model_name>\S*)[/*]$', catalog_profiles.catalog_profile_model),
     re_path(r'^catalog/profile/(?P<manufacture_id>\d+)-(?P<manufacture_name>\S*)[/*]$',
-            catalog.catalog_profile_manufacture),
+            catalog_profiles.catalog_profile_manufacture),
     # --- --- Каталог серий типового строительства
     re_path(r'^catalog/seria[/*]$', catalog.catalog_seria),
     re_path(r'^catalog/seria/(?P<seria_name_translit>[^/]*)/all(?P<seria_id>\d+)[/*]$', catalog.catalog_seria_info),
@@ -99,6 +98,7 @@ urlpatterns = [
     re_path(r'^service/make_rating[/*]$', service.make_rating),
 
 ]
+
 
 if DEBUG:
     #  ___    ____      _              _____         _ _              _____             _
